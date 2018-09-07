71c7337be7e488b89f30b2d9cd4b5cbd6d3cdd2a
Romain Grecourt <romain.grecourt@oracle.com>
2018-09-07 14:25:45 -0700

    bump the version to 1.0.1-SNAPSHOT

--- etc/scripts/release.sh
+++ etc/scripts/release.sh
@@ -170,8 +170,7 @@ update_version(){
     -DgenerateBackupPoms=false \
     -DnewVersion="${FULL_VERSION}" \
     -Dproperty=helidon.version \
-    -DprocessAllModules=true \
-    -Pexamples
+    -DprocessAllModules=true
 
   # Invoke prepare hook
   if [ -n "${PREPARE_HOOKS}" ]; then
