b9a98027f1eaf75ca8fc231ecd8f7db6ce5eb72d
Romain Grecourt <romain.grecourt@oracle.com>
2020-03-02 15:33:22 -0800

    update doxia renderer to trigger report rendering

--- sitegen-maven-plugin/src/main/java/io/helidon/build/sitegen/maven/DoxiaSiteRenderer.java
+++ sitegen-maven-plugin/src/main/java/io/helidon/build/sitegen/maven/DoxiaSiteRenderer.java
@@ -16,44 +16,72 @@
 package io.helidon.build.sitegen.maven;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.io.Writer;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
 
 import io.helidon.build.sitegen.RenderingException;
 import io.helidon.build.sitegen.Site;
 
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.doxia.site.decoration.DecorationModel;
-import org.apache.maven.doxia.siterenderer.DocumentContent;
+import org.apache.maven.doxia.siterenderer.DefaultSiteRenderer;
 import org.apache.maven.doxia.siterenderer.DocumentRenderer;
 import org.apache.maven.doxia.siterenderer.Renderer;
 import org.apache.maven.doxia.siterenderer.RendererException;
 import org.apache.maven.doxia.siterenderer.RenderingContext;
 import org.apache.maven.doxia.siterenderer.SiteRenderingContext;
-import org.apache.maven.doxia.siterenderer.sink.SiteRendererSink;
+import org.apache.maven.plugins.site.render.ReportDocumentRenderer;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.component.annotations.Component;
-import org.codehaus.plexus.logging.AbstractLogEnabled;
+import org.codehaus.plexus.util.IOUtil;
+import org.codehaus.plexus.util.WriterFactory;
 
 /**
  * Doxia site renderer.
  */
 @Component(role = Renderer.class)
-public class DoxiaSiteRenderer extends AbstractLogEnabled implements Renderer {
+public class DoxiaSiteRenderer extends DefaultSiteRenderer {
 
     @Override
     public void render(Collection<DocumentRenderer> documents, SiteRenderingContext context, File outputDirectory)
             throws RendererException, IOException {
+
+        for (DocumentRenderer docRenderer : documents) {
+            if (!(docRenderer instanceof ReportDocumentRenderer)) {
+                continue;
+            }
+            RenderingContext renderingContext = docRenderer.getRenderingContext();
+            File outputFile = new File(outputDirectory, docRenderer.getOutputName());
+            File inputFile = new File(renderingContext.getBasedir(), renderingContext.getInputName());
+            boolean modified = !outputFile.exists()
+                    || (inputFile.lastModified() > outputFile.lastModified())
+                    || (context.getDecoration().getLastModified() > outputFile.lastModified());
+
+            if (modified || docRenderer.isOverwrite()) {
+                if (!outputFile.getParentFile().exists()) {
+                    outputFile.getParentFile().mkdirs();
+                }
+
+                if (getLogger().isDebugEnabled()) {
+                    getLogger().debug("Generating " + outputFile);
+                }
+
+                Writer writer = null;
+                try {
+                    if (!docRenderer.isExternalReport()) {
+                        writer = WriterFactory.newWriter(outputFile, context.getOutputEncoding());
+                    }
+                    docRenderer.renderDocument(writer, this, context);
+                } finally {
+                    IOUtil.close(writer);
+                }
+            } else {
+                if (getLogger().isDebugEnabled()) {
+                    getLogger().debug(inputFile + " unchanged, not regenerating...");
+                }
+            }
+        }
 
         Properties properties = new Properties();
         Map<String, ?> templateProps = context.getTemplateProperties();
@@ -87,68 +115,11 @@ public class DoxiaSiteRenderer extends AbstractLogEnabled implements Renderer {
     }
 
     @Override
-    public void generateDocument(Writer writer, SiteRendererSink sink, SiteRenderingContext siteRenderingContext)
-            throws RendererException {
-    }
-
-    @Override
-    public void mergeDocumentIntoSite(Writer writer, DocumentContent content, SiteRenderingContext siteRenderingContext)
-            throws RendererException {
-    }
-
-    private SiteRenderingContext createSiteRenderingContext(Map<String, ?> attributes, DecorationModel decoration,
-            String defaultWindowTitle, Locale locale) {
-
-        SiteRenderingContext context = new SiteRenderingContext();
-        context.setTemplateProperties(attributes);
-        context.setLocale(locale);
-        context.setDecoration(decoration);
-        context.setDefaultWindowTitle(defaultWindowTitle);
-        return context;
-    }
-
-    @Override
-    public SiteRenderingContext createContextForSkin(Artifact skin, Map<String, ?> attributes, DecorationModel decoration,
-            String defaultWindowTitle, Locale locale) throws RendererException, IOException {
-
-        return createSiteRenderingContext(attributes, decoration, defaultWindowTitle, locale);
-    }
-
-    @Override
-    public SiteRenderingContext createContextForTemplate(File templateFile, Map<String, ?> attributes, DecorationModel decoration,
-            String defaultWindowTitle, Locale locale) throws MalformedURLException {
-
-        SiteRenderingContext context = createSiteRenderingContext(attributes, decoration, defaultWindowTitle, locale);
-        context.setTemplateName(templateFile.getName());
-        context.setTemplateClassLoader(new URLClassLoader(new URL[]{templateFile.getParentFile().toURI().toURL()}));
-        return context;
-    }
-
-    @Override
     public void copyResources(SiteRenderingContext siteRenderingContext, File resourcesDirectory, File outputDirectory)
             throws IOException {
     }
 
     @Override
     public void copyResources(SiteRenderingContext siteRenderingContext, File outputDirectory) throws IOException {
-    }
-
-    @Override
-    public Map<String, DocumentRenderer> locateDocumentFiles(SiteRenderingContext siteRenderingContext)
-            throws IOException, RendererException {
-
-        return Collections.emptyMap();
-    }
-
-    @Override
-    public Map<String, DocumentRenderer> locateDocumentFiles(SiteRenderingContext siteRenderingContext, boolean editable)
-            throws IOException, RendererException {
-
-        return Collections.emptyMap();
-    }
-
-    @Override
-    public void renderDocument(Writer writer, RenderingContext docRenderingContext, SiteRenderingContext siteContext)
-            throws RendererException, FileNotFoundException, UnsupportedEncodingException {
     }
 }
