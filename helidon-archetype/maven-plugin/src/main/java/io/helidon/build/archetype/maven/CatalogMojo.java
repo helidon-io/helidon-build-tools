eade14edf0f7336d1137886983a56bd4b4b7d8e7
Romain Grecourt <romain.grecourt@oracle.com>
2020-06-15 22:57:55 -0700

    Update archetype:catalog to create a zip archive that contains the catalog descriptor and the archetype jar files

--- helidon-archetype/maven-plugin/src/main/java/io/helidon/build/archetype/maven/CatalogMojo.java
+++ helidon-archetype/maven-plugin/src/main/java/io/helidon/build/archetype/maven/CatalogMojo.java
@@ -18,7 +18,10 @@ package io.helidon.build.archetype.maven;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import io.helidon.build.archetype.engine.ArchetypeCatalog;
@@ -26,10 +29,21 @@ import io.helidon.build.archetype.engine.ArchetypeCatalog;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.archiver.Archiver;
+import org.codehaus.plexus.archiver.manager.ArchiverManager;
+import org.codehaus.plexus.archiver.manager.NoSuchArchiverException;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.resolution.ArtifactResult;
 
 import static io.helidon.build.archetype.maven.MojoHelper.MUSTACHE_EXT;
 import static io.helidon.build.archetype.maven.MojoHelper.renderMustacheTemplate;
@@ -44,7 +58,7 @@ public class CatalogMojo extends AbstractMojo {
     /**
      * The catalog filename.
      */
-    private static final String CATALOG_FILENAME = "catalog.xml";
+    private static final String CATALOG_FILENAME = "archetype-catalog.xml";
 
     /**
      * The Maven project this mojo executes on.
@@ -53,10 +67,33 @@ public class CatalogMojo extends AbstractMojo {
     private MavenProject project;
 
     /**
+     * Manager used to look up Archiver/UnArchiver implementations.
+     */
+    @Component
+    private ArchiverManager archiverManager;
+
+    /**
+     * The entry point to Aether.
+     */
+    @Component
+    private RepositorySystem repoSystem;
+
+    /**
+     * The current repository/network configuration of Maven.
+     */
+    @Parameter(defaultValue = "${repositorySystemSession}", readonly = true)
+    private RepositorySystemSession repoSession;
+
+    /**
+     * The project remote repositories to use.
+     */
+    @Parameter(defaultValue = "${project.remoteProjectRepositories}", readonly = true)
+    private List<RemoteRepository> remoteRepos;
+
+    /**
      * The project build output directory. (e.g. {@code target/})
      */
-    @Parameter(defaultValue = "${project.build.directory}",
-            readonly = true, required = true)
+    @Parameter(defaultValue = "${project.build.directory}", readonly = true, required = true)
     private File outputDirectory;
 
     /**
@@ -70,6 +107,12 @@ public class CatalogMojo extends AbstractMojo {
      */
     @Parameter(defaultValue = "${basedir}/catalog.xml", required = true)
     private File archetypeCatalog;
+
+    /**
+     * The archetype catalog archive.
+     */
+    @Parameter(defaultValue = "${project.build.directory}/${project.build.finalName}.zip", required = true)
+    private File catalogArchive;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
@@ -85,8 +128,47 @@ public class CatalogMojo extends AbstractMojo {
         } else {
             throw new MojoFailureException("No catalog.xml or catalog.xml.mustache found");
         }
-        verifyCatalog(archetypeCatalog);
-        project.getArtifact().setFile(archetypeCatalog);
+
+        getLog().info("Resolving archetypes...");
+        ArchetypeCatalog catalog = readCatalog(archetypeCatalog);
+        List<File> archetypeFiles = new LinkedList<>();
+        for (ArchetypeCatalog.ArchetypeEntry archetypeEntry : catalog.entries()) {
+            archetypeFiles.add(resolveArchetype(archetypeEntry));
+        }
+
+        getLog().info("Creating archetype catalog archive: " + catalogArchive);
+        try {
+            Archiver archiver = archiverManager.getArchiver(catalogArchive);
+            archiver.addFile(archetypeCatalog, CATALOG_FILENAME);
+            for (File archetypeFile : archetypeFiles) {
+                archiver.addFile(archetypeFile, archetypeFile.getName());
+            }
+            archiver.setDestFile(catalogArchive);
+            archiver.createArchive();
+        } catch (NoSuchArchiverException | IOException ex) {
+            throw new MojoExecutionException(ex.getMessage(), ex);
+        }
+        project.getArtifact().setFile(catalogArchive);
+    }
+
+    /**
+     * Resolve an archetype artifact.
+     *
+     * @param archetypeEntry archetype entry
+     * @return File
+     */
+    private File resolveArchetype(ArchetypeCatalog.ArchetypeEntry archetypeEntry) {
+        ArtifactRequest request = new ArtifactRequest();
+        request.setArtifact(new DefaultArtifact(archetypeEntry.groupId(), archetypeEntry.artifactId(), null,
+                "jar", archetypeEntry.version()));
+        request.setRepositories(remoteRepos);
+        ArtifactResult result = null;
+        try {
+            result = repoSystem.resolveArtifact(repoSession, request);
+        } catch (ArtifactResolutionException ex) {
+            throw new RuntimeException(ex);
+        }
+        return result.getArtifact().getFile();
     }
 
     /**
@@ -95,9 +177,9 @@ public class CatalogMojo extends AbstractMojo {
      * @param catalogFile catalog file
      * @throws MojoExecutionException if an error occurred while reading the catalog
      */
-    private static void verifyCatalog(File catalogFile) throws MojoExecutionException {
+    private static ArchetypeCatalog readCatalog(File catalogFile) throws MojoExecutionException {
         try {
-            ArchetypeCatalog.read(new FileInputStream(catalogFile));
+            return ArchetypeCatalog.read(new FileInputStream(catalogFile));
         } catch (FileNotFoundException ex) {
             throw new MojoExecutionException(ex.getMessage(), ex);
         }
