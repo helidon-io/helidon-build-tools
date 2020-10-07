27c3f188da1023b9efb0af80ef688a73ace58d35
Bryan Atsatt <bryan.atsatt@oracle.com>
2020-10-07 16:38:36 -0700

    Align plugin version machinery across build and dev commands.

--- helidon-cli/impl/src/main/java/io/helidon/build/cli/impl/DevCommand.java
+++ helidon-cli/impl/src/main/java/io/helidon/build/cli/impl/DevCommand.java
@@ -16,23 +16,18 @@
 
 package io.helidon.build.cli.impl;
 
-import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 import io.helidon.build.cli.harness.Command;
 import io.helidon.build.cli.harness.CommandContext;
 import io.helidon.build.cli.harness.CommandContext.Verbosity;
-import io.helidon.build.cli.harness.Config;
 import io.helidon.build.cli.harness.Creator;
 import io.helidon.build.cli.harness.Option.Flag;
 import io.helidon.build.cli.harness.Option.KeyValue;
 import io.helidon.build.util.AnsiConsoleInstaller;
 import io.helidon.build.util.Log;
 import io.helidon.build.util.MavenCommand;
-import io.helidon.build.util.MavenVersion;
-import io.helidon.build.util.ProjectConfig;
-import io.helidon.build.util.RequirementFailure;
 import io.helidon.build.util.Strings;
 import io.helidon.build.util.StyleFunction;
 
@@ -52,7 +47,6 @@ import static io.helidon.build.util.DevLoopMessages.DEV_LOOP_HEADER;
 import static io.helidon.build.util.DevLoopMessages.DEV_LOOP_MESSAGE_PREFIX;
 import static io.helidon.build.util.DevLoopMessages.DEV_LOOP_START;
 import static io.helidon.build.util.DevLoopMessages.DEV_LOOP_STYLED_MESSAGE_PREFIX;
-import static io.helidon.build.util.MavenVersion.toMavenVersion;
 import static io.helidon.build.util.StyleFunction.Bold;
 import static io.helidon.build.util.StyleFunction.BoldBlue;
 import static io.helidon.build.util.StyleFunction.BoldBrightGreen;
@@ -69,7 +63,6 @@ public final class DevCommand extends BaseCommand {
     private static final String TERMINAL_MODE_PROP_PREFIX = "-Ddev.terminalMode=";
     private static final String APP_JVM_ARGS_PROP_PREFIX = "-Ddev.appJvmArgs=";
     private static final String APP_ARGS_PROP_PREFIX = "-Ddev.appArgs=";
-    private static final String HELIDON_CLI_PLUGIN_VERSION_PROP_PREFIX = "-Dversion.plugin.helidon-cli=";
     private static final String CLI_MAVEN_PLUGIN = "io.helidon.build-tools:helidon-cli-maven-plugin";
     private static final String DEV_GOAL_SUFFIX = ":dev";
     private static final String MAVEN_LOG_LEVEL_START = "[";
@@ -81,15 +74,15 @@ public final class DevCommand extends BaseCommand {
     private static final String STARTING = BoldBrightGreen.apply("starting");
     private static final String EXITING = BoldBrightGreen.apply("exiting");
     private static final String DEFAULT_DEBUG_PORT = "5005";
-    private static final Set<String> SUSPEND_FALSE = Set.of("n", "no", "f", "false");
     private static final String DEBUG_ARGS = "-agentlib:jdwp=transport=dt_socket,server=y,suspend=%s,address=*:%s";
 
     private final CommonOptions commonOptions;
     private final boolean clean;
     private final boolean fork;
-    private final String pluginVersion;
     private final String appJvmArgs;
     private final String appArgs;
+    private final String pluginVersion;
+    private final boolean useCurrentPluginVersion;
     private TerminalModeOutput terminalModeOutput;
 
     /**
@@ -98,16 +91,14 @@ public final class DevCommand extends BaseCommand {
      * @param commonOptions Common options.
      */
     DevCommand(CommonOptions commonOptions) {
-        this(commonOptions, true, false, null,
-             null, null, false, null, false);
+        this(commonOptions, true, false, null, null, false, null,
+             false, null, false);
     }
 
     @Creator
     DevCommand(CommonOptions commonOptions,
                @Flag(name = "clean", description = "Perform a clean before the first build") boolean clean,
                @Flag(name = "fork", description = "Fork mvn execution") boolean fork,
-               @KeyValue(name = "version", description = "helidon-cli-maven-plugin version", visible = false)
-                       String pluginVersion,
                @KeyValue(name = "app-jvm-args", description = "JVM args used when starting the application")
                        String appJvmArgs,
                @KeyValue(name = "app-args", description = "Application args used when starting the application")
@@ -117,24 +108,20 @@ public final class DevCommand extends BaseCommand {
                @KeyValue(name = "app-debug-port", description = "Specify application debugger port")
                        String appDebugPort,
                @Flag(name = "app-debug-no-wait", description = "Do not wait for debugger on application start")
-                       boolean appDebugNoWait) {
+                       boolean appDebugNoWait,
+               @KeyValue(name = "plugin-version", description = "helidon-cli-maven-plugin version", visible = false)
+                       String pluginVersion,
+               @Flag(name = "current", description = "Use the build version as the helidon-cli-maven-plugin version",
+                       visible = false)
+                       boolean useCurrentPluginVersion) {
         super(commonOptions, true);
         this.commonOptions = commonOptions;
         this.clean = clean;
         this.fork = fork;
-        this.pluginVersion = defaultPluginVersion(pluginVersion);
         this.appJvmArgs = appJvmArgs(appJvmArgs, appDebug, appDebugPort, appDebugNoWait);
         this.appArgs = appArgs;
-    }
-
-    private static String defaultPluginVersion(String suppliedVersion) {
-        if (suppliedVersion == null) {
-            // Use build version if this is not a release
-            MavenVersion version = toMavenVersion(Config.buildVersion());
-            return version.isQualified() ? version.toString() : null;
-        } else {
-            return suppliedVersion;
-        }
+        this.pluginVersion = pluginVersion;
+        this.useCurrentPluginVersion = useCurrentPluginVersion;
     }
 
     private static String appJvmArgs(String appJvmArgs, boolean appDebug, String appDebugPort, boolean appDebugNoWait) {
@@ -144,12 +131,13 @@ public final class DevCommand extends BaseCommand {
     }
 
     private static String debugArgs(boolean appDebug, String appDebugPort, boolean appDebugNoWait) {
-        if (!appDebug && appDebugPort == null && !appDebugNoWait) {
-            return null;
-        } else {
-            String port = Strings.isValid(appDebugPort) ? appDebugPort : DEFAULT_DEBUG_PORT;
+        boolean havePort = Strings.isValid(appDebugPort);
+        if (appDebug || havePort || appDebugNoWait) {
+            String port = havePort ? appDebugPort : DEFAULT_DEBUG_PORT;
             String suspend = appDebugNoWait ? "n" : "y";
             return String.format(DEBUG_ARGS, suspend, port);
+        } else {
+            return null;
         }
     }
 
@@ -164,14 +152,15 @@ public final class DevCommand extends BaseCommand {
 
         // Dev goal
 
-        String cliPluginVersion = pluginVersion == null ? cliPluginVersion() : pluginVersion;
         String devGoal = CLI_MAVEN_PLUGIN;
         String cliPluginVersionProperty = null;
+        String defaultPluginVersion = defaultHelidonPluginVersion(pluginVersion, useCurrentPluginVersion);
+        String cliPluginVersion = cliPluginVersion(defaultPluginVersion);
         if (cliPluginVersion != null) {
             Log.verbose("Using CLI plugin version %s", cliPluginVersion);
             devGoal += ":" + cliPluginVersion;
             // Pass along the version so that the loop can specify it when doing full builds
-            cliPluginVersionProperty = HELIDON_CLI_PLUGIN_VERSION_PROP_PREFIX + cliPluginVersion;
+            cliPluginVersionProperty = HELIDON_CLI_PLUGIN_VERSION_PROPERTY_PREFIX + cliPluginVersion;
         }
         devGoal += DEV_GOAL_SUFFIX;
 
@@ -247,66 +236,6 @@ public final class DevCommand extends BaseCommand {
         System.out.println(StyleFunction.Red.apply(line));
     }
 
-    private String cliPluginVersion() {
-        if (pluginVersion == null) {
-
-            // No plugin version was specified via command line options, so we need to select it
-            // from metadata based on the Helidon version for the project.
-
-            // First, try to find the Helidon version from the project config (.helidon file). Note
-            // that if this file was deleted and no build has occurred, a minimal project config is
-            // generated in our preconditions, which will attempt to find and store the Helidon
-            // version by reading the pom and checking for a Helidon parent pom. Though a Helidon
-            // parent pom will normally be present, it is not required so we may not find it in the
-            // config; in that case, fallback to the latest version. If we fail to get that, use our
-            // build version.
-
-            Metadata meta = metadata();
-            ProjectConfig projectConfig = projectConfig();
-            String helidonVersionProperty = projectConfig.property(ProjectConfig.HELIDON_VERSION);
-            String buildVersion = Config.buildVersion();
-            MavenVersion helidonVersion;
-            if (helidonVersionProperty == null) {
-                try {
-                    helidonVersion = meta.latestVersion();
-                    Log.debug("helidon.version missing in %s, using latest: %s", projectConfig.file(), helidonVersion);
-                } catch (Exception e) {
-                    helidonVersion = toMavenVersion(buildVersion);
-                    Log.debug("unable to lookup latest Helidon version, using build version %s: %s",
-                              buildVersion, e.getMessage());
-                }
-            } else {
-                helidonVersion = toMavenVersion(helidonVersionProperty);
-            }
-
-            // Short circuit if Helidon version is qualified since metadata only exists for releases
-
-            if (helidonVersion.isQualified()) {
-                Log.debug("Helidon version %s not a release, using current CLI version %s", helidonVersion, buildVersion);
-                return buildVersion;
-            }
-
-            // Now lookup and return the CLI plugin version (which will short circuit if Helidon version is
-            // prior to the existence of the CLI plugin).
-
-            try {
-                Log.debug("using Helidon version %s to find CLI plugin version", helidonVersion);
-                return meta.cliPluginVersion(helidonVersion, true).toString();
-            } catch (Plugins.PluginFailed e) {
-                Log.debug("unable to lookup CLI plugin version for Helidon version %s: %s", helidonVersion, e.getMessage());
-            } catch (RequirementFailure e) {
-                Log.debug("CLI plugin version not specified for Helidon version %s: %s", helidonVersion);
-            } catch (Exception e) {
-                Log.debug("unable to lookup CLI plugin version for Helidon version %s: %s", helidonVersion, e.toString());
-            }
-
-            // We failed so return null to let the project pom dictate the version (which will fail if not configured)
-
-            return null;
-        } else {
-            return pluginVersion;
-        }
-    }
 
     /**
      * A stateful filter/transform that cleans up output from {@code DevLoop}.
