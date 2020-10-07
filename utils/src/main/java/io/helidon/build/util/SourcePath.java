0d1c2d3c713ae0f2b0f6ba73a4e4b282136bdf9d
Romain Grecourt <romain.grecourt@oracle.com>
2020-10-06 20:02:31 -0700

    fix checkstyle errors

--- utils/src/main/java/io/helidon/build/util/SourcePath.java
+++ utils/src/main/java/io/helidon/build/util/SourcePath.java
@@ -26,7 +26,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
