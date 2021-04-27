0128ed2925b9dcd3826cd68c481df111198ac14c
Romain Grecourt <romain.grecourt@oracle.com>
2021-04-26 20:25:44 -0700

    fix checkstyle error

--- helidon-archetype/maven-plugin/src/main/java/io/helidon/build/archetype/maven/postgenerate/EngineFacade.java
+++ helidon-archetype/maven-plugin/src/main/java/io/helidon/build/archetype/maven/postgenerate/EngineFacade.java
@@ -23,7 +23,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
