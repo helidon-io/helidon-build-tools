0694f68dca0580f3e5c51a10e4b7ebf209a04e0e
Romain Grecourt <romain.grecourt@oracle.com>
2022-02-02 11:36:48 -0800

    use a loop to fully drain the buffered reader in LineReader.drain() (#643)

--- utils/src/main/java/io/helidon/build/util/LineReader.java
+++ utils/src/main/java/io/helidon/build/util/LineReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -85,7 +85,7 @@ class LineReader {
     void drain() {
         try {
             try {
-                if (reader.ready()) {
+                while (reader.ready()) {
                     readLines(false);
                     if (!remaining.isEmpty()) {
                         String line = remaining;
