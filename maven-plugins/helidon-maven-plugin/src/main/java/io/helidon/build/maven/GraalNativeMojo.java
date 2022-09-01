1ce1a00c3393d548c140cfd7e2de2963f4b618cd
tvallin <thibault.vallin@oracle.com>
2022-09-01 11:11:57 +0200

    fix checkstyle
    Signed-off-by: tvallin <thibault.vallin@oracle.com>

--- maven-plugins/helidon-maven-plugin/src/main/java/io/helidon/build/maven/GraalNativeMojo.java
+++ maven-plugins/helidon-maven-plugin/src/main/java/io/helidon/build/maven/GraalNativeMojo.java
@@ -23,11 +23,9 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
