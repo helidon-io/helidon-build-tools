ca330fc2693b2863e1c03e5cd913202a01191aa4
tvallin <thibault.vallin@oracle.com>
2023-01-18 13:30:45 +0100

    refactor scripts
    Signed-off-by: tvallin <thibault.vallin@oracle.com>

--- cli/impl/src/main/assembly/helidon.bat
+++ cli/impl/src/main/assembly/helidon.bat
@@ -24,30 +24,29 @@ if "%JAVACMD%"=="" set JAVACMD=java
 for %%i in ("%~dp0..") do set "BASEDIR=%%~fi"
 
 set JARFILE=%BASEDIR%\lib\helidon.jar
-set args=
-set localDebug=
+set ARGS=
 
 for %%x in (%*) do (
-   call :parseArgs %%~x
+   call :parse_args %%~x
 )
 
-if NOT "%isDebug%"=="" (
+if NOT "%IS_DEBUG%"=="" (
     echo "[WARNING] Use HELIDON_JAVA_OPS environment property to setup JVM arguments"
     echo "[DEBUG] Distribution located at : %BASEDIR%"
     echo "[DEBUG] Using java command : %JAVACMD%"
-    echo "[DEBUG] Command executed : %JAVACMD% %HELIDON_JAVA_OPTS% -jar %JARFILE% %args%"
+    echo "[DEBUG] Command executed : %JAVACMD% %HELIDON_JAVA_OPTS% -jar %JARFILE% %ARGS%"
 )
 
-%JAVACMD% %HELIDON_JAVA_OPTS% -jar %JARFILE% %args%
+%JAVACMD% %HELIDON_JAVA_OPTS% -jar %JARFILE% %ARGS%
 
 ENDLOCAL
 goto :eof
 
-:parseArgs
+:parse_args
 if "--cli-debug" == "%~1" (
-    set isDebug="true"
+    set IS_DEBUG="true"
 ) else (
-    set args=%args% %~1
+    set ARGS=%ARGS% %~1
 )
 exit /b
 
