4ece0cbaede860250e813ea9c166310e96e6c3e7
Romain Grecourt <romain.grecourt@oracle.com>
2023-08-04 15:22:48 -0700

    Switch to Smallrye Jandex to align with Helidon. (#956) (#973)
    Co-authored-by: Tomas Langer <tomas.langer@oracle.com>

--- linker/src/main/java/module-info.java
+++ linker/src/main/java/module-info.java
@@ -25,7 +25,7 @@ module helidon.linker {
 
     requires jdk.jlink;
     requires jdk.jdeps;
-    requires jandex;
+    requires org.jboss.jandex;
     requires org.fusesource.jansi;
     requires org.objectweb.asm;
     requires io.helidon.build.common;
