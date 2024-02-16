646426d6b7f035d11c53e100ce18322530cfd59b
Joe Di Pol <joe.dipol@oracle.com>
2024-02-16 15:00:55 -0800

    Add support for owasp dependency check

---
+++ etc/scripts/owasp-dependency-check.sh
@@ -0,0 +1,64 @@
+#!/bin/bash -e
+#
+# Copyright (c) 2020, 2023 Oracle and/or its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Path to this script
+[ -h "${0}" ] && readonly SCRIPT_PATH="$(readlink "${0}")" || readonly SCRIPT_PATH="${0}"
+
+WS_DIR=$(cd $(dirname -- "${SCRIPT_PATH}") ; cd ../.. ; pwd -P)
+
+    on_error() {
+        CODE="${?}" && \
+        set +x && \
+        printf "[ERROR] Error(code=%s) occurred at %s:%s command: %s\n" \
+            "${CODE}" "${BASH_SOURCE}" "${LINENO}" "${BASH_COMMAND}"
+    }
+
+    # Error handling setup
+    # Arguments: $1 - error handler name (optional, default name is 'on_error')
+    error_trap_setup() {
+        # trace ERR through pipes
+        set -o pipefail || true
+        # trace ERR through commands and functions
+        set -o errtrace || true
+        # exit the script if any statement returns a non-true return value
+        set -o errexit || true
+        # Set error handler
+        trap "${1:-on_error}" ERR
+    }
+
+# Setup error handling using default settings (defined in includes/error_handlers.sh)
+error_trap_setup
+
+readonly RESULT_FILE=$(mktemp -t XXXdependency-check-result)
+
+die() { cat ${RESULT_FILE} ; echo "Dependency report in ${WS_DIR}/target" ; echo "${1}" ; exit 1 ;}
+
+if [ "${PIPELINE}" = "true" ] ; then
+    # If in pipeline do a priming build before scan
+    mvn ${MAVEN_ARGS} -f ${WS_DIR}/pom.xml clean install -DskipTests
+fi
+
+# Setting NVD_API_KEY is not required but improves behavior of NVD API throttling
+
+mvn ${MAVEN_ARGS} -Dorg.slf4j.simpleLogger.defaultLogLevel=WARN org.owasp:dependency-check-maven:aggregate \
+        -f ${WS_DIR}/pom.xml \
+        -Dtop.parent.basedir="${WS_DIR}" \
+        -Dnvd-api-key=${NVD_API_KEY} \
+        > ${RESULT_FILE} || die "Error running the Maven command"
+
+grep -i "One or more dependencies were identified with known vulnerabilities" ${RESULT_FILE} \
+    && die "CVE SCAN ERROR" || echo "CVE SCAN OK"
