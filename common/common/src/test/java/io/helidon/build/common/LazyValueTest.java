4f8dd490db1639f6b158e7b53ef9b41d651cf87a
Romain Grecourt <romain.grecourt@oracle.com>
2024-07-24 23:03:48 -0700

    Fix LazyValueTest (#1049)
    - Update LazyValue to be almost identical to the original implementation

--- common/common/src/test/java/io/helidon/build/common/LazyValueTest.java
+++ common/common/src/test/java/io/helidon/build/common/LazyValueTest.java
@@ -31,7 +31,6 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
@@ -42,7 +41,7 @@ class LazyValueTest {
 
     @Test
     void testInitialized() {
-        LazyValue<String> lazyValue = new LazyValue<>(() -> "value");
+        LazyValue<String> lazyValue = new LazyValue<>("value");
         assertThat(lazyValue.get(), is("value"));
         assertThat(lazyValue.get(), is("value"));
     }
@@ -64,76 +63,71 @@ class LazyValueTest {
     @Test
     void testInitRaceWithBadSupplier() throws InterruptedException, ExecutionException {
         AtomicInteger counter = new AtomicInteger();
+        CountDownLatch l1 = new CountDownLatch(1);
+        CountDownLatch l2 = new CountDownLatch(1);
         LazyValue<String> lazyValue = new LazyValue<>(() -> {
-            if (counter.getAndIncrement() == 0) {
-                sleep();
-                throw new RuntimeException("error!");
+            try {
+                l1.countDown();
+                l2.await();
+                if (counter.getAndIncrement() == 0) {
+                    throw new RuntimeException("error!");
+                }
+                return Thread.currentThread().getName();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                System.out.println("after supplier.get()");
             }
-            return Thread.currentThread().getName();
         });
 
         Deque<Future<String>> futures = new ArrayDeque<>();
         ExecutorService executorService = executorService();
 
-        CountDownLatch latch1 = new CountDownLatch(1);
+        futures.add(executorService.submit(lazyValue::get));
+        l1.await();
         futures.add(executorService.submit(() -> {
-            latch1.countDown();
+            l2.countDown();
             return lazyValue.get();
         }));
 
-        latch1.await();
-        futures.add(executorService.submit(lazyValue::get));
-
-        Future<String> first = futures.pop();
-        ExecutionException ex = assertThrows(ExecutionException.class, first::get);
+        ExecutionException ex = assertThrows(ExecutionException.class, futures.pop()::get);
         assertThat(ex.getCause(), is(instanceOf(RuntimeException.class)));
         assertThat(ex.getCause().getMessage(), is("error!"));
 
-        while (!futures.isEmpty()) {
-            Future<String> future = futures.pop();
+        for (Future<String> future : futures) {
             String value = future.get();
-            assertThat(value, is(nullValue()));
-        }
-
-        CountDownLatch latch2 = new CountDownLatch(1);
-        futures.add(executorService.submit(() -> {
-            latch2.countDown();
-            return lazyValue.get();
-        }));
-        latch2.await();
-
-        futures.add(executorService.submit(lazyValue::get));
-
-        while (!futures.isEmpty()) {
-            Future<String> future = futures.pop();
-            String value = future.get();
-            assertThat(value, is("test-3"));
+            assertThat(value, is("test-2"));
         }
 
         String value = lazyValue.get();
-        assertThat(value, is("test-3"));
+        assertThat(value, is("test-2"));
     }
 
     @Test
     void testInitRace() throws InterruptedException, ExecutionException {
+        CountDownLatch l1 = new CountDownLatch(1);
+        CountDownLatch l2 = new CountDownLatch(3);
         LazyValue<String> lazyValue = new LazyValue<>(() -> {
-            sleep();
-            return Thread.currentThread().getName();
+            try {
+                l1.countDown();
+                l2.await();
+                return Thread.currentThread().getName();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         });
 
         List<Future<String>> futures = new ArrayList<>();
         ExecutorService executorService = executorService();
 
-        CountDownLatch latch = new CountDownLatch(1);
-        futures.add(executorService.submit(() -> {
-            latch.countDown();
-            return lazyValue.get();
-        }));
-        latch.await();
-
         futures.add(executorService.submit(lazyValue::get));
-        futures.add(executorService.submit(lazyValue::get));
-        futures.add(executorService.submit(lazyValue::get));
+        l1.await();
+        for (int i = 0; i < 3; i++) {
+            futures.add(executorService.submit(() -> {
+                l2.countDown();
+                return lazyValue.get();
+            }));
+        }
 
         for (Future<String> future : futures) {
             String value = future.get();
@@ -147,13 +141,5 @@ class LazyValueTest {
     private static ExecutorService executorService() {
         AtomicInteger counter = new AtomicInteger(1);
         return Executors.newFixedThreadPool(4, r -> new Thread(null, r, "test-" + counter.getAndIncrement()));
-    }
-
-    private static void sleep() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
