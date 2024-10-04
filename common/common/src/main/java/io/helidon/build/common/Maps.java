f37fe22395b3585273d726e013745e31f0da706f
Romain Grecourt <romain.grecourt@oracle.com>
2024-10-04 14:37:20 -0700

    fix copyright and checkstyle

--- common/common/src/main/java/io/helidon/build/common/Maps.java
+++ common/common/src/main/java/io/helidon/build/common/Maps.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2023 Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2024 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -434,7 +434,7 @@ public class Maps {
         return map.entrySet().stream()
                   .filter(entry-> keyFilter.test(entry.getKey()))
                   .collect(Collectors.toMap(
-                          entry->keyMapper.apply(entry.getKey()),
-                          entry->entry.getValue().stream().map(valueMapper).collect(Collectors.toList())));
+                          entry-> keyMapper.apply(entry.getKey()),
+                          entry-> entry.getValue().stream().map(valueMapper).collect(Collectors.toList())));
     }
 }
