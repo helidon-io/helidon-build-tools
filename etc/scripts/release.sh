c7ae1347ea6ef4f34d89cf87e72a5cbbda41b47e
Romain Grecourt <romain.grecourt@oracle.com>
2025-07-17 14:22:55 -0700

    Central Release Updates
    Sync-up the release process with the main Helidon repository.
    
    - Update release workflow to split release into stage and upload
    - Update workflows to avoid double builds on release branches
    - Update release.sh to have only update_version and create_tag sub commands
    - Add etc/scripts/setup-gpg.sh
    - Update maven-deploy-plugin to 3.1.1 (to support the file URI used in stage)
    - Update etc/scripts/shellcheck.sh to support Apple Silicon (upgrade to 0.10.0)
    - workaround an issue with the build-cache extension and attached artifacts in archetype/engine-v2
    
    update version back to 4.0.0-SNAPSHOT

--- etc/scripts/release.sh
+++ etc/scripts/release.sh
@@ -20,12 +20,26 @@ set -o errtrace || true # trace ERR through commands and functions
 set -o errexit || true  # exit the script if any statement returns a non-true return value
 
 on_error(){
-    CODE="${?}" && \
-    set +x && \
-    printf "[ERROR] Error(code=%s) occurred at %s:%s command: %s\n" \
-        "${CODE}" "${BASH_SOURCE[0]}" "${LINENO}" "${BASH_COMMAND}"
+  CODE="${?}" && \
+  set +x && \
+  printf "[ERROR] Error(code=%s) occurred at %s:%s command: %s\n" \
+    "${CODE}" "${BASH_SOURCE[0]}" "${LINENO}" "${BASH_COMMAND}" >&2
 }
 trap on_error ERR
+
+# Path to this script
+if [ -h "${0}" ] ; then
+  SCRIPT_PATH="$(readlink "${0}")"
+else
+  # shellcheck disable=SC155
+  SCRIPT_PATH="${0}"
+fi
+readonly SCRIPT_PATH
+
+# Path to the root of the workspace
+# shellcheck disable=SC2046
+WS_DIR=$(cd $(dirname -- "${SCRIPT_PATH}") ; cd ../.. ; pwd -P)
+readonly WS_DIR
 
 usage(){
     cat <<EOF
@@ -34,11 +48,10 @@ DESCRIPTION: Helidon Release Script
 
 USAGE:
 
-$(basename "${0}") [ --build-number=N ] CMD
+$(basename "${0}") --version=V CMD
 
   --version=V
-        Override the version to use.
-        This trumps --build-number=N
+        The version to use.
 
   --help
         Prints the usage and exits.
@@ -48,162 +61,171 @@ $(basename "${0}") [ --build-number=N ] CMD
     update_version
         Update the version in the workspace
 
-    release_version
-        Print the release version
+    get_version
+        Get the current version
 
     create_tag
-        Create and push a release tag
-
-    release_build
-        Perform a release build
-
+        Create and and push a release tag
 EOF
 }
 
 # parse command line args
 ARGS=( )
 while (( ${#} > 0 )); do
-    case ${1} in
-    "--version="*)
-        VERSION=${1#*=}
-        shift
-        ;;
-    "--help")
-        usage
-        exit 0
-        ;;
-    "update_version"|"release_version"|"create_tag"|"release_build")
-        COMMAND="${1}"
-        shift
-        ;;
-    *)
-        ARGS+=( "${1}" )
-        shift
-        ;;
-    esac
+  case ${1} in
+  "--version="*)
+    VERSION=${1#*=}
+    shift
+    ;;
+  "--help")
+    usage
+    exit 0
+    ;;
+  "update_version"|"create_tag"|"get_version")
+    COMMAND="${1}"
+    shift
+    ;;
+  *)
+    ARGS+=( "${1}" )
+    shift
+    ;;
+  esac
 done
 readonly ARGS
 readonly COMMAND
+
+# copy stdout as fd 6 and redirect stdout to stderr
+# this allows us to use fd 6 for returning data
+exec 6>&1 1>&2
 
 if [ -z "${COMMAND+x}" ] ; then
   echo "ERROR: no command provided"
   exit 1
 fi
 
-# Path to this script
-if [ -h "${0}" ] ; then
-    SCRIPT_PATH="$(readlink "${0}")"
-else
-    SCRIPT_PATH="${0}"
-fi
-readonly SCRIPT_PATH
+case ${COMMAND} in
+"update_version")
+  if [ -z "${VERSION}" ] ; then
+    echo "ERROR: version required" >&2
+    usage
+    exit 1
+  fi
+  ;;
+"create_tag"|"get_version")
+  # no-op
+  ;;
+"")
+  echo "ERROR: no command provided" >&2
+  usage
+  exit 1
+  ;;
+*)
+  echo "ERROR: unknown command ${COMMAND}" >&2
+  usage
+  exit 1
+  ;;
+esac
 
-# Path to the root of the workspace
-# shellcheck disable=SC2046
-WS_DIR=$(cd $(dirname -- "${SCRIPT_PATH}") ; cd ../.. ; pwd -P)
-readonly WS_DIR
-
-# copy stdout as fd 6 and redirect stdout to stderr
-# this allows us to use fd 6 for returning data
-exec 6>&1 1>&2
-
-current_version(){
-    # shellcheck disable=SC2086
-    mvn ${MVN_ARGS} -q \
-        -f "${WS_DIR}"/pom.xml \
-        -Dexec.executable="echo" \
-        -Dexec.args="\${project.version}" \
-        --non-recursive \
-        org.codehaus.mojo:exec-maven-plugin:1.3.1:exec
+current_version() {
+  awk 'BEGIN {FS="[<>]"} ; /<version>/ {print $3; exit 0}' "${WS_DIR}"/pom.xml
 }
 
-release_version(){
-    local current_version
-    current_version=$(current_version)
-    echo "${current_version%-*}"
+# arg1: pattern
+# arg2: include pattern
+search() {
+  set +o pipefail
+  grep "${1}" -Er . --include "${2}" | cut -d ':' -f 1 | xargs git ls-files | sort | uniq
+}
+
+replace() {
+  local pattern value replace include
+  while (( ${#} > 0 )); do
+    case ${1} in
+    "--pattern="*)
+      pattern=${1#*=}
+      shift
+      ;;
+    "--include="*)
+      include=${1#*=}
+      shift
+      ;;
+    "--replace="*)
+      replace=${1#*=}
+      shift
+      ;;
+    "--value="*)
+      value=${1#*=}
+      shift
+      ;;
+    *)
+      echo "Unsupported argument: ${1}" >&2
+      return 1
+      ;;
+    esac
+  done
+
+  if [ -z "${replace}" ] && [ -n "${value}" ] ; then
+    replace=${pattern/\.\*/${value}}
+  fi
+
+  for file in $(search "${pattern}" "${include}"); do
+    echo "Updating ${file}"
+    sed -e s@"${pattern}"@"${replace}"@g "${file}" > "${file}.tmp"
+    mv "${file}.tmp" "${file}"
+  done
 }
 
 update_version(){
-    local version
-    version=${1-${VERSION}}
-    if [ -z "${version+x}" ] ; then
-        echo "ERROR: version required"
-        usage
-        exit 1
-    fi
+  local version current_version
 
-    # shellcheck disable=SC2086
-    mvn ${MVN_ARGS} "${ARGS[@]}" \
-        -f "${WS_DIR}"/pom.xml versions:set versions:set-property \
-        -DgenerateBackupPoms="false" \
-        -DnewVersion="${version}" \
-        -Dproperty="helidon.version" \
-        -DprocessFromLocalAggregationRoot="false" \
-        -DupdateMatchingVersions="false"
+  version=${1-${VERSION}}
+  if [ -z "${version+x}" ] ; then
+    echo "ERROR: version required" >&2
+    usage
+    exit 1
+  fi
+
+  # find current version
+  current_version=$(current_version)
+
+  # update poms
+  replace \
+    --pattern="<version>${current_version}</version>" \
+    --replace="<version>${version}</version>" \
+    --include="pom.xml"
 }
 
-create_tag(){
-    local git_branch version
+create_tag() {
+  local git_branch version current_version
 
-    version=$(release_version)
-    git_branch="release/${version}"
+  current_version=$(current_version)
+  version=${current_version%-SNAPSHOT}
+  git_branch="release/${version}"
 
-    # Use a separate branch
-    git branch -D "${git_branch}" > /dev/null 2>&1 || true
-    git checkout -b "${git_branch}"
+  # Use a separate branch
+  git branch -D "${git_branch}" > /dev/null 2>&1 || true
+  git checkout -b "${git_branch}"
 
-    # Invoke update_version
-    update_version "${version}"
+  # Invoke update_version
+  update_version "${version}"
 
-    # Git user info
-    git config user.email || git config --global user.email "info@helidon.io"
-    git config user.name || git config --global user.name "Helidon Robot"
+  # Git user info
+  git config user.email || git config --global user.email "info@helidon.io"
+  git config user.name || git config --global user.name "Helidon Robot"
 
-    # Commit version changes
-    git commit -a -m "Release ${version}"
+  # Commit version changes
+  git commit -a -m "Release ${version}"
 
-    # Create and push a git tag
-    git tag -f "${version}"
-    git push --force origin refs/tags/"${version}":refs/tags/"${version}"
+  # Create and push a git tag
+  git tag -f "${version}"
+  git push --force origin refs/tags/"${version}":refs/tags/"${version}"
 
-    echo "tag=refs/tags/${version}" >&6
+  echo "version=${version}" >&6
+  echo "tag=refs/tags/${version}" >&6
 }
 
-release_build(){
-    local tmpfile version
-
-    # Bootstrap credentials from environment
-    if [ -n "${MAVEN_SETTINGS}" ] ; then
-        tmpfile=$(mktemp XXXXXXsettings.xml)
-        echo "${MAVEN_SETTINGS}" > "${tmpfile}"
-        MVN_ARGS="${MVN_ARGS} -s ${tmpfile}"
-    fi
-    if [ -n "${GPG_PRIVATE_KEY}" ] ; then
-        tmpfile=$(mktemp XXXXXX.key)
-        echo "${GPG_PRIVATE_KEY}" > "${tmpfile}"
-        gpg --allow-secret-key-import --import --no-tty --batch "${tmpfile}"
-        rm "${tmpfile}"
-    fi
-    if [ -n "${GPG_PASSPHRASE}" ] ; then
-        echo "allow-preset-passphrase" >> ~/.gnupg/gpg-agent.conf
-        gpg-connect-agent reloadagent /bye
-        GPG_KEYGRIP=$(gpg --with-keygrip -K | grep "Keygrip" | head -1 | awk '{print $3}')
-        /usr/lib/gnupg/gpg-preset-passphrase --preset "${GPG_KEYGRIP}" <<< "${GPG_PASSPHRASE}"
-    fi
-
-    # Perform local deployment to filesystem
-    # shellcheck disable=SC2086
-    mvn ${MVN_ARGS} "${ARGS[@]}" \
-        deploy \
-        -Prelease \
-        -DskipTests \
-        -DaltDeploymentRepository=":::file://${PWD}/staging"
-
-    # Upload artifacts to Sonatype Central Publishing Portal
-    version=$(release_version)
-    "${WS_DIR}/etc/scripts/upload.sh" upload_release \
-                --dir="staging" \
-                --description="Helidon Build Tools v${version}"
+get_version() {
+  echo "version=$(current_version)" >&6
 }
 
 # Invoke command
